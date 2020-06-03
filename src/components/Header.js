83c87f6453f96ab4bab91696329138ce7654ec2b
Glitch (starter-react-template) <none>
2020-06-03 09:51:10 +0000

    🐲💒 Checkpoint ./src/App.js:15532837/893 ./src/App.css:15532837/984 ./src/components/Header.js:15532837/922 ./src/components/FollowFriendListItem.js:15532837/817 ./src/styles/FollowFriendListItem.css:15532837/465 ./src/styles/Header.css:15532837/101

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import '../styles/Header.css'
+import FollowFriendListItem from './FollowFriendListItem';
+
+class Header extends Component {
+    constructor() {
+      super();
+      this.users = [
+        "Phong0809",
+        "Thinh0606",
+        "Thong0303"
+      ];
+    }
+    
+    render() {
+        return <div class="recommended-friends">
+                  <div class="recommended-friends-header">
+                    <div class="recommended-friends-header-left">
+                      <p>Gợi ý cho bạn</p>
+                    </div>
+                    <div class="recommended-friends-header-right">
+                      <p>Xem tất cả</p>
+                    </div> 
+                  </div> 
+                    {
+                      this.users.map((user, index) => <FollowFriendListItem user={user} key={index}/>)
+                    }
+               </div>;
+    }
+}
+
+export default Header;
